ab300315abe7d9e1f4804c44ddb5fcf8ca362c17
Alimustoofaa <softhouse.ali@gmail.com>
2021-08-09 22:07:55 +0700

    add logger

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,3 +2,5 @@ from .process_license_plate import resize
 from .process_license_plate import read_text
 from .process_license_plate import detect_char
 from .process_license_plate import detection_object
+
+from .logger import logging
