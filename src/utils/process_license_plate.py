f0bef3ce72d289b4142ad928326491bcbe7685c9
Alimustoofaa <softhouse.ali@gmail.com>
2021-08-12 10:08:47 +0700

    fix indentation

--- src/utils/process_license_plate.py
+++ src/utils/process_license_plate.py
@@ -11,59 +11,59 @@ model           = LicensePlateDetection()
 ocr             = OpticalCharacterRecognition()
 
 def detection_object(image):
-    '''
-    Detection license plate
-    and filter clasess, confidence
-    Args:
-        image(np.array): image for cropped
-    return:
-        result(tuple): (
-                image_cropped(np.array): image croped,
-                confidence(float): confidence level,
-                bbox(list): bbox detection [x_min, y_min, x_max, y_max]
-            )
+	'''
+	Detection license plate
+	and filter clasess, confidence
+	Args:
+		image(np.array): image for cropped
+	return:
+		result(tuple): (
+				image_cropped(np.array): image croped,
+				confidence(float): confidence level,
+				bbox(list): bbox detection [x_min, y_min, x_max, y_max]
+			)
 
-    '''
-    result_detection = model.prediction(image)
-    license_plate = model.filter_and_crop(
-        img=image, results=result_detection, min_confidence=0.0
-    )
-    if len(license_plate[0]) >=1 and license_plate[1] > 0 and len(license_plate[2]) == 4:
-        logging.info(f'Got license plate detection confidence : {round(license_plate[1], 2)*100} %')
-    else: logging.info(f'License plate not found')
-    return license_plate
+	'''
+	result_detection = model.prediction(image)
+	license_plate = model.filter_and_crop(
+		img=image, results=result_detection, min_confidence=0.0
+	)
+	if len(license_plate[0]) >=1 and license_plate[1] > 0 and len(license_plate[2]) == 4:
+		logging.info(f'Got license plate detection confidence : {round(license_plate[1], 2)*100} %')
+	else: logging.info(f'License plate not found')
+	return license_plate
 
 def classification_vehicle(image):
-    '''
-    Classification vehicle
-    and filter clasess, confidence
-    Args:
-        image(np.array): image for classification
-    Return:
-        result(tuple): (
-            clasess(str): clases name
-            confidence(float): confidence level
-            bbox(list): bbox object [x_min, y_min, x_max, y_max]
-        )
-    '''
-    result_classification = classification.prediction(image)
-    vehicle_type = classification.filter_and_crop(
-        results=result_classification, min_confidence=0.0
-    )
-    if vehicle_type[0] and vehicle_type[1] and vehicle_type[2]:
-        logging.info(f'Got classification {vehicle_type[2]} confidence : {round(vehicle_type[0], 2)*100} %')
-    else: logging.info(f'Not found object classification')
-    return vehicle_type
-    
+	'''
+	Classification vehicle
+	and filter clasess, confidence
+	Args:
+		image(np.array): image for classification
+	Return:
+		result(tuple): (
+			clasess(str): clases name
+			confidence(float): confidence level
+			bbox(list): bbox object [x_min, y_min, x_max, y_max]
+		)
+	'''
+	result_classification = classification.prediction(image)
+	vehicle_type = classification.filter_and_crop(
+		results=result_classification, min_confidence=0.0
+	)
+	if vehicle_type[0] and vehicle_type[1] and vehicle_type[2]:
+		logging.info(f'Got classification {vehicle_type[2]} confidence : {round(vehicle_type[0], 2)*100} %')
+	else: logging.info(f'Not found object classification')
+	return vehicle_type
+	
 def resize(image, height_percent=180, width_percent=180):
 	'''
 	resize image by percent
-    Args:
-        image(np.array): image
-        height_percent(int): percentage height
-        width_percent(int): percentage width
-    Return:
-        image(np.array): image resized
+	Args:
+		image(np.array): image
+		height_percent(int): percentage height
+		width_percent(int): percentage width
+	Return:
+		image(np.array): image resized
 	'''
 	height = int(image.shape[0] * height_percent / 100)
 	width = int(image.shape[1] * width_percent / 100)
@@ -73,61 +73,61 @@ def resize(image, height_percent=180, width_percent=180):
 	return new_img
 
 def detect_char(image, output=False):
-    '''
-    Detection charcters text in image
-    Args:
-        image(np.array): image.
-        output(boolean): output detection default (output=False)
-    Return:
-        result(boolean|list): result detection character
-    '''
-    detected_char = ocr.detect_char(image)
-    if detected_char[0]:
-        logging.info(f'Found text in image : {" ".join([str(i) for i in detected_char[0]])}')
-    else:
-        logging.info(f'Not found text in image')
+	'''
+	Detection charcters text in image
+	Args:
+		image(np.array): image.
+		output(boolean): output detection default (output=False)
+	Return:
+		result(boolean|list): result detection character
+	'''
+	detected_char = ocr.detect_char(image)
+	if detected_char[0]:
+		logging.info(f'Found text in image : {" ".join([str(i) for i in detected_char[0]])}')
+	else:
+		logging.info(f'Not found text in image')
 
-    if output:
-        results = detected_char[0][0]
-    else: 
-        if detected_char[0]: results = True
-        else: results = False
-    return results
+	if output:
+		results = detected_char[0][0]
+	else: 
+		if detected_char[0]: results = True
+		else: results = False
+	return results
 
 def read_text(image, position_text='horizontal', clasess_name='license_plate'):
-    '''
-    Set methods and value config ocr
-    methods view schema/config_ocr.py
-    Args:
-        image(np.array): image for read text
-        position_text(str): position text vertical/horizontal (default=vertical)
-        clasess_name(str): clasess name read text (default=license_plate)
-    Retrun:
-        result(list): [([[28, 32], [52, 32], [52, 64], [28, 64]], 'text', 0.9846626687831872)]
-    '''
-    if position_text == 'horizontal':
-        config = ConfigOcr(
-            beam_width      = 8,
-            batch_size      = 10,
-            text_threshold  = 0.5,
-            link_threshold  = 0.9,
-            low_text        = 0.4,
-            slope_ths       = 0.9,
-            mag_ratio 		= 1,
-            add_margin		= 0.5,
-            width_ths       = 0.5
-        )
-    elif position_text == 'vertical':
-        config = ConfigOcr(
-            batch_size  	= 10,
-            text_threshold 	= 0.2,
-            link_threshold 	= 0.9,
-            low_text 		= 0.4,
-            add_margin		= 0
-        )
+	'''
+	Set methods and value config ocr
+	methods view schema/config_ocr.py
+	Args:
+		image(np.array): image for read text
+		position_text(str): position text vertical/horizontal (default=vertical)
+		clasess_name(str): clasess name read text (default=license_plate)
+	Retrun:
+		result(list): [([[28, 32], [52, 32], [52, 64], [28, 64]], 'text', 0.9846626687831872)]
+	'''
+	if position_text == 'horizontal':
+		config = ConfigOcr(
+			beam_width      = 8,
+			batch_size      = 10,
+			text_threshold  = 0.5,
+			link_threshold  = 0.9,
+			low_text        = 0.4,
+			slope_ths       = 0.9,
+			mag_ratio 		= 1,
+			add_margin		= 0.5,
+			width_ths       = 0.5
+		)
+	elif position_text == 'vertical':
+		config = ConfigOcr(
+			batch_size  	= 10,
+			text_threshold 	= 0.2,
+			link_threshold 	= 0.9,
+			low_text 		= 0.4,
+			add_margin		= 0
+		)
 
-    results = ocr.ocr_image(image=image, config=config)
-    if position_text == 'horizontal': results.sort(reverse=False)
-    else : results = results
-    logging.info(f'Ocr {clasess_name} : {" ".join([i[1] for i in results])}')
-    return results
+	results = ocr.ocr_image(image=image, config=config)
+	if position_text == 'horizontal': results.sort(reverse=False)
+	else : results = results
+	logging.info(f'Ocr {clasess_name} : {" ".join([i[1] for i in results])}')
+	return results
