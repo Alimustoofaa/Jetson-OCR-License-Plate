71e10e3e1db3aec92235425351a26fc365859b03
Alimustoofaa <softhouse.ali@gmail.com>
2021-09-03 13:22:21 +0700

    add thread server and camera

--- server.py
+++ server.py
@@ -1,8 +1,29 @@
+import time
+import uvicorn
+import threading
+import contextlib
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
 from web.app.routes import list_vehicles, streaming
+
+class Server(uvicorn.Server):
+	def install_signal_handlers(self):
+		pass
+
+	@contextlib.contextmanager
+	def run_in_thread(self):
+		thread = threading.Thread(target=self.run)
+		thread.start()
+		try:
+			while not self.started:
+				time.sleep(1e-3)
+			yield
+		finally:
+			self.should_exit = True
+			thread.join()
 
 app = FastAPI()
 
@@ -15,6 +36,7 @@ app.add_middleware(
 )
 
 app.mount("/dist", StaticFiles(directory="web/assets/dist"), name="dist")
+app.mount("/captures", StaticFiles(directory="captures"), name="captures")
 templates = Jinja2Templates(directory="web/pages/")
 
 app.include_router(list_vehicles.router)
