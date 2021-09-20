a5d8f26bf314c35ec6cf7154bcd99c51485bea51
Alimustoofaa <softhouse.ali@gmail.com>
2021-09-20 10:11:03 +0700

    read adrucam csi

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -4,8 +4,15 @@ import subprocess
 from config import COMMAND_CAMERA_PROPERTY
 
 class CameraCSI:
+	'''
+	Read camera module cv2 and
+	run command v4l2 config
+	Args:
+		num_device(int): number device camera
+	'''
 	def __init__(self, num_device):
-		self.camera = cv2.VideoCapture(num_device, cv2.CAP_V4L2)
+		self.num_device = num_device
+		self.camera = cv2.VideoCapture(self.num_device, cv2.CAP_V4L2)
 		self.camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('U', 'Y', 'V', 'Y'))
 		# self.cap.set(cv2.CAP_PROP_CONVERT_RGB, self.cap.convert2rgb)
 		self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -19,4 +26,5 @@ class CameraCSI:
 			ret, frame = self.camera.read()
 
 	def camera_arducam(self):
+		if not self.camera.isOpened(): self.__init__(self.num_device)
 		return self.camera
